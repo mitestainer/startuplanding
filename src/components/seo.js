9b9f66f438fc9dbe830043dda3780110ee6d03ab
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-07-25 21:46:27 -0300

    Fix metatags

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
+import socialImg from 'assets/social.png'
 
 export default function SEO({
-  description = 'startup product landing page',
-  author = 'JSM',
+  description = 'Top Quality Digital Products to Explore - Check out our website to find great software products and deals! If you need a website or a web application this is the place to be!',
+  author = 'Matheus Mitestainer',
   meta,
-  title = 'startup landing title',
+  title,
 }) {
   const metaData = [
     {
-      name: `description`,
+      property: `description`,
       content: description,
+    },
+    {
+      property: `og:image`,
+      content: socialImg,
     },
     {
       property: `og:title`,
@@ -25,28 +30,32 @@ export default function SEO({
       content: `website`,
     },
     {
-      name: `twitter:card`,
-      content: `summary`,
+      property: `twitter:card`,
+      content: `summary_large_image`,
     },
     {
-      name: `twitter:creator`,
+      property: `twitter:creator`,
       content: author,
     },
     {
-      name: `twitter:title`,
+      property: `twitter:image`,
+      content: socialImg,
+    },
+    {
+      property: `twitter:title`,
       content: title,
     },
     {
-      name: `twitter:description`,
+      property: `twitter:description`,
       content: description,
     },
   ].concat(meta);
-  
+
   return (
     <Head>
       <title>{title}</title>
-      {metaData.map(({ name, content }, i) => (
-        <meta key={i} name={name} content={content} />
+      {metaData.map(({ property, content }, i) => (
+        <meta key={i} property={property} content={content} />
       ))}
     </Head>
   );
